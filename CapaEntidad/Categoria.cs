367ff91f9617cb2d517db86f2e503d0f60995a4e
Steven Mendez <stevenampaiz@gmail.com>
2022-02-02 20:46:48 -0600

    Parte 04 finalizada. Agregadas las capas Datos, Negocio y Entidad.

---
+++ CapaEntidad/Categoria.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class Categoria
+    {
+        public int IdCategoria { get; set; }
+
+        public string Descrípcion { get; set; }
+
+        public bool Actovp { get; set; }
+    }
+}
