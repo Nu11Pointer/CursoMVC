488fa9706bf005af43b729f73e18d58c09f5ba61
Steven Mendez <stevenampaiz@gmail.com>
2022-02-19 11:20:34 -0600

    Parte-11 terminada. Crear, Leer, Actualizar Usuarios.

--- CapaDatos/CD_Usuarios.cs
+++ CapaDatos/CD_Usuarios.cs
@@ -112,10 +112,9 @@ namespace CapaDatos
                     cmd.Parameters.AddWithValue("Nombres", obj.Nombres);
                     cmd.Parameters.AddWithValue("Apellidos", obj.Apellidos);
                     cmd.Parameters.AddWithValue("Correo", obj.Correo);
-                    cmd.Parameters.AddWithValue("Clave", obj.Clave);
                     cmd.Parameters.AddWithValue("Activo", obj.Activo);
-                    cmd.Parameters.Add("Resultado", SqlDbType.Int).Direction = ParameterDirection.Output;
                     cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("Resultado", SqlDbType.Int).Direction = ParameterDirection.Output;
 
                     oconexion.Open();
 
--- CapaPresentacionAdmin/Controllers/HomeController.cs
+++ CapaPresentacionAdmin/Controllers/HomeController.cs
@@ -20,11 +20,29 @@ namespace CapaPresentacionAdmin.Controllers
             return View();
         }
 
-[HttpGet]
-public JsonResult ListarUsuarios()
-{
-    List<Usuario> oLista = new CN_Usuarios().Listar();
-    return Json(new { data = oLista }, JsonRequestBehavior.AllowGet);
-}
+        [HttpGet]
+        public JsonResult ListarUsuarios()
+        {
+            List<Usuario> oLista = new CN_Usuarios().Listar();
+            return Json(new { data = oLista }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult GuardarUsuario(Usuario objeto)
+        {
+            object resultado;
+            string mensaje = string.Empty;
+
+            if (objeto.IdUsuario == 0)
+            {
+                resultado = new CN_Usuarios().Registrar(objeto, out mensaje);
+            }
+            else
+            {
+                resultado = new CN_Usuarios().Editar(objeto, out mensaje);
+            }
+
+            return Json(new { resultado = resultado, mensaje = mensaje }, JsonRequestBehavior.AllowGet);
+        }
     }
 }
