45b9f4b281b4307203698a7a762d34ebedf23641
Steven Mendez <stevenampaiz@gmail.com>
2022-02-19 20:46:39 -0600

    Parte-12 finalizada.

--- CapaPresentacionAdmin/App_Start/BundleConfig.cs
+++ CapaPresentacionAdmin/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace CapaPresentacionAdmin
 {
@@ -14,6 +13,8 @@ namespace CapaPresentacionAdmin
                       "~/Scripts/fontawesome/all.min.js",
                       "~/Scripts/DataTables/jquery.dataTables.js",
                       "~/Scripts/DataTables/dataTables.responsive.js",
+                      "~/Scripts/loadingoverlay/loadingoverlay.min.js",
+                      "~/Scripts/sweetalert.min.js",
                       "~/Scripts/scripts.js"
                       ));
 
@@ -23,7 +24,8 @@ namespace CapaPresentacionAdmin
             bundles.Add(new StyleBundle("~/Content/css").Include(
                 "~/Content/site.css",
                 "~/Content/DataTables/css/jquery.dataTables.css",
-                "~/Content/DataTables/css/responsive.dataTables.css"
+                "~/Content/DataTables/css/responsive.dataTables.css",
+                "~/Content/sweetalert.css"
                 ));
         }
     }
